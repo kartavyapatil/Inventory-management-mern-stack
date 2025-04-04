a376431964cd47e255a0e3b7a7c8e8255ca0d7b3
kartavyapatil <145218094+kartavyapatil@users.noreply.github.com>
2025-04-04 22:18:25 +0530

    Update user.query.js

--- frontend/src/query/user.query.js
+++ frontend/src/query/user.query.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const consumerApi = createApi({
     reducerPath: 'consumerApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000/api/v1' }),
+    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BACKEND_URL}),
     tagTypes:['GetAllConsumer',"GetAllConsumerdetails"],
     endpoints: (builder) => ({
         registerconsumer: builder.mutation({
