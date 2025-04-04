414883f12cdf25fdb02e8488a157980eb4baa5b7
kartavyapatil <145218094+kartavyapatil@users.noreply.github.com>
2025-04-04 22:16:50 +0530

    Update Auth.query.js

--- frontend/src/query/Auth.query.js
+++ frontend/src/query/Auth.query.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const AuthApi = createApi({
     reducerPath: 'AuthApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000/api/v1' }),
+    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BACKEND_URL }),
     endpoints: (builder) => ({
         registerUser: builder.mutation({
             query: (obj) => ({
